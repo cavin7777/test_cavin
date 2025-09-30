56413a07fbfbb447b3664bd16064e51531d71c1d
cavin7777 <caven7777@gmail.com>
2025-09-30 11:26:45 +0400

    test

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
 print ("cavin")
+
+a = "Python is cool"
+print(a[7:-5])
+print(a[-2])
+print(f"{98} Battery street")
+
+name = "Jane"
+age = 25
+print("Hello, %s!" %name)
+print("Hello %s, you're %s years old." %(name, age))
+
+print("Hello %(name1)s You're %(age1)s years old." % {"name1": "Cavin", "age1": 30})
+
+print("Balance: $%.2f" % 5425.9292)
