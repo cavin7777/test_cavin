c3f14553722fab76cf4b365c57891bb7a2a99d99
cavin7777 <caven7777@gmail.com>
2025-10-01 13:10:18 +0400

    all

--- 0_python_strings.py
+++ 0_python_strings.py
@@ -19,3 +19,19 @@ print('He is called "Cavin"')
 a = "Hello"
 print(a)
 
+# Multiline Strings
+# You can assign a multiline string to a variable by using three quotes:
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+# Or three single quotes:
+
+b = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(b) 
--- test.py
+++ test.py
@@ -13,3 +13,6 @@ print("Hello %s, you're %s years old." %(name, age))
 print("Hello %(name1)s You're %(age1)s years old." % {"name1": "Cavin", "age1": 30})
 
 print("Balance: $%.2f" % 5425.9292)
+
+s = 'supercalifragilisticexpialidocious'
+print(len(s))
