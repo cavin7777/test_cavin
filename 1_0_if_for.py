c1f536db25914292171dc17bd664354d20c45b05
cavin7777 <caven7777@gmail.com>
2025-10-01 14:32:57 +0400

    all

--- 1_0_if_for.py
+++ 1_0_if_for.py
@@ -10,6 +10,7 @@ elif x == 1:
     print('Single')
 else:
     print('More')
+
 ## FOR statement
 
 # Measure some strings:
@@ -42,4 +43,20 @@ for i in range(len(a)):
 # SUM
 sum1 = sum(range(4))
 print(sum1)
+
+## break and continue Statements
+
+# The break statement breaks out of the innermost enclosing for or while loop:
+for n in range(2, 10):
+    for x in range(2, n):
+        if n % x == 0:
+            print(f'{n} equals {x} * {n//x}')
+            break
+
+# The continue statement continues with the next iteration of the loop:
+for num in range(2, 10):
+    if num % 2 == 0:
+        print(f'Foundan even number {num}')
+        continue
+    print(f'Found an odd number {num}')
 
