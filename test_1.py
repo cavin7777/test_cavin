6a93d80bdaa11c3ef4a7df2f2214d7f8b632555f
cavin7777 <caven7777@gmail.com>
2025-10-01 10:18:29 +0400

    0_python

---
+++ test_1.py
@@ -0,0 +1,6 @@
+# Ask the user to input their name and assign it to a variable named "name"
+name = input('What is your name ')
+
+# Print out hello followed by name enterred
+
+print('Hello', name)
